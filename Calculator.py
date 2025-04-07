5311115e69774ff05a248ca8b05ffa566c03eb3a
pranjal-8757 <pranjalghaste@gmail.com>
2025-04-07 20:40:26 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+# calculator.py
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def exponent(a, b):
+    return a ** b
+
+
+print("Addition:", add(2, 3))
+print("Subtraction:", subtract(5, 2))
+print("Exponent:", exponent(2, 3))
